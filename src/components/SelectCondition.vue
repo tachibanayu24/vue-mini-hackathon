e6ce2a43c04f19d447c6f436314783969a206c06
小野田 奈生 <6521850@K0189.shareddev.local>
2019-03-18 16:38:38 +0900

    選択肢の微修正

--- src/components/SelectCondition.vue
+++ src/components/SelectCondition.vue
@@ -71,12 +71,12 @@ export default {
       price: [
         { label: "~500円", value: 1 },
         { label: "501~1000円", value: 2 },
-        { label: "1000円～", value: 3 }
+        { label: "1000円~", value: 3 }
       ],
       distance: [
         { label: "~5分", value: 1 },
         { label: "6~10分", value: 2 },
-        { label: "10分～", value: 3 }
+        { label: "10分~", value: 3 }
       ],
       category: [
         { label: "和食", value: 1 },
