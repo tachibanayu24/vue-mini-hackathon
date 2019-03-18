58e16f1398b6e4d0519d03c726f5e4eeeac9d7d4
tachibanayu24 <tachibanayu24@gmail.com>
2019-03-18 15:06:04 +0900

    ロゴを編集

--- src/views/MkpApp.vue
+++ src/views/MkpApp.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="mkp-app">
-    <div class="word">
-      <span>お</span>
-      <span>ひ</span>
-      <span>る</span>
-      <span>〜</span>
-      <span>れ</span>
-      <span>っ</span>
-      <span>と</span>
-    </div>
+    <el-row>
+      <el-col :span="12">
+        <div class="logo-img">
+          <!-- <img src="../assets/logo.png" alt> -->
+          <h1>おひる～れっと</h1>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="logo-anime">
+          <div id="container">Make your
+            <div id="flip">
+              <div>
+                <div>Lunch🍔</div>
+              </div>
+              <div>
+                <div>Lunch🍣</div>
+              </div>
+              <div>
+                <div>Lunch🍜</div>
+              </div>
+            </div>AweSoMe!
+          </div>
+        </div>
+      </el-col>
+    </el-row>
 
     <Otenki/>
     <SelectCondition/>
@@ -32,159 +48,108 @@ export default {
     StoreList
   }
 };
-
-window.onload = () => {
-  let spans = document.querySelectorAll(".word span");
-  spans.forEach((span, idx) => {
-    span.addEventListener("click", e => {
-      e.target.classList.add("active");
-    });
-    span.addEventListener("animationend", e => {
-      e.target.classList.remove("active");
-    });
-
-    // Initial animation
-    setTimeout(() => {
-      span.classList.add("active");
-    }, 750 * (idx + 1));
-  });
-}
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css?family=Anton|Roboto");
+@import url("https://fonts.googleapis.com/css?family=Roboto:700");
+@import url(https://fonts.googleapis.com/earlyaccess/notosansjp.css);
 
-.word {
-  font-family: "Anton", sans-serif;
-  perspective: 1000px;
-  perspective-origin: 200px 40px;
+.logo-img {
+  font-family: "Noto Sans JP";
+    font-size: 9em;
+
+  color: #666;
+  font-weight: 900;
+  text-shadow: -1px 0 #bfc0c0, 0 1px #bfc0c0, 1px 0 #bfc0c0, 0 -1px #bfc0c0;
+  margin: auto;
+  float: right;
 }
 
-.word span {
-  cursor: pointer;
-  display: inline-block;
-  font-size: 100px;
-  user-select: none;
-  line-height: 0.8;
+.logo-anime {
+  margin: 0px 20px;
+  font-family: "Roboto";
 }
 
-.word span:nth-child(1).active {
-  animation: balance 1.5s ease-out;
-  transform-origin: 0% 100% 0px;
+#container {
+  color: #999;
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: bold;
+  width: 100%;
+  bottom: 45%;
+  display: block;
+  text-align: left;
 }
 
-@keyframes balance {
-  0%,
-  100% {
-    transform: rotate(0deg);
-  }
-
-  30%,
-  60% {
-    transform: rotate(-45deg);
-  }
+#flip {
+  height: 50px;
+  overflow: hidden;
 }
 
-.word span:nth-child(2).active {
-  animation: shrinkjump 1s ease-in-out;
-  transform-origin: bottom center;
-}
-
-@keyframes shrinkjump {
-  10%,
-  35% {
-    transform: scale(2, 0.2) translate(0, 0);
-  }
-
-  45%,
-  50% {
-    transform: scale(1) translate(0, -150px);
-  }
-
-  80% {
-    transform: scale(1) translate(0, 0);
-  }
-}
-
-.word span:nth-child(3).active {
-  animation: falling 2s ease-out;
-  transform-origin: bottom center;
-}
-
-@keyframes falling {
-  12% {
-    transform: rotateX(240deg);
-  }
-
-  24% {
-    transform: rotateX(150deg);
-  }
-
-  36% {
-    transform: rotateX(200deg);
-  }
-
-  48% {
-    transform: rotateX(175deg);
-  }
-
-  60%,
-  85% {
-    transform: rotateX(180deg);
-  }
-
-  100% {
-    transform: rotateX(0deg);
-  }
-}
-
-.word span:nth-child(4).active {
-  animation: rotate 1s ease-out;
-}
-
-@keyframes rotate {
-  20%,
-  80% {
-    transform: rotateY(180deg);
-  }
-
-  100% {
-    transform: rotateY(360deg);
-  }
-}
-
-.word span:nth-child(5).active {
-  animation: toplong 1.5s linear;
-}
-
-@keyframes toplong {
-  10%,
-  40% {
-    transform: translateY(-48vh) scaleY(1);
-  }
-
-  90% {
-    transform: translateY(-48vh) scaleY(4);
-  }
-}
-
-/* Other styles */
-body {
-  background-color: skyblue;
+#flip > div > div {
   color: #fff;
-  display: flex;
-  font-family: "Roboto", sans-serif;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  height: 100vh;
-  margin: 0;
+  padding: 4px 12px;
+  height: 45px;
+  margin-bottom: 45px;
+  display: inline-block;
 }
 
-.fixed {
+#flip div:first-child {
+  animation: show 5s linear infinite;
+}
+
+#flip div div {
+  background: #42c58a;
+}
+
+#flip div:first-child div {
+  background: #4ec7f3;
+}
+
+#flip div:last-child div {
+  background: #dc143c;
+}
+
+@keyframes show {
+  0% {
+    margin-top: -270px;
+  }
+
+  5% {
+    margin-top: -170px;
+  }
+
+  33% {
+    margin-top: -170px;
+  }
+
+  38% {
+    margin-top: -80px;
+  }
+
+  66% {
+    margin-top: -80px;
+  }
+
+  71% {
+    margin-top: 0px;
+  }
+
+  99.99% {
+    margin-top: 0px;
+  }
+
+  100% {
+    margin-top: -270px;
+  }
+}
+
+p {
   position: fixed;
-  top: 40px;
-  left: 50%;
-  transform: translateX(-50%);
+  width: 100%;
+  bottom: 30px;
+  font-size: 12px;
+  color: #999;
+  margin-top: 200px;
 }
 </style>
